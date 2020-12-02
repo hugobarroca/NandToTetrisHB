bf811d0f79b63941f3d76f2d1b4e287fdd5aa3ae
Hugo Barroca <hugobarroca98@gmail.com>
2020-12-02 20:37:10 +0000

    Started Project 07. Added code stub for the VM Translator.

--- Project 06/Hack Assembler/src/main/Main.java
+++ Project 06/Hack Assembler/src/main/Main.java
@@ -12,11 +12,8 @@ public class Main {
 			return;
 		}
 		Addresser addresser = new Addresser(new File(args[0]));
-		String a = addresser.runAddresser();
-		Parser parser = new Parser(a);
-//		Parser parser = new Parser(args[0]);
+		Parser parser = new Parser(addresser.runAddresser());
 		Codifier codifier = new Codifier(parser.readString());
-//		Codifier codifier = new Codifier(parser.readFile());
 		codifier.codeInstructionsToBinary();
 		codifier.writeOutputToFile(args[0].replaceAll(".asm", ".hack"));
 
