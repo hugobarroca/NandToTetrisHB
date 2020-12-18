e7571628c7d0e2ea0ccec823af329214e1239a42
Hugo Barroca <hugobarroca98@gmail.com>
2020-12-18 21:03:04 +0000

    Fixed neg and not commands trying to access the wrong memory address.

--- Project 07/Python_VM_Translator/code_writer.py
+++ Project 07/Python_VM_Translator/code_writer.py
@@ -33,9 +33,9 @@ class VMCodeWriter:
         if arithmetic_command.startswith('or'):
             self.output_content += '// or\n@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=D&M\n'
         if arithmetic_command.startswith('neg'):
-            self.output_content += '// neg\n@SP\nA=M\nM=-M\n'
+            self.output_content += '// neg\n@SP\nA=M-1\nM=-M\n'
         if arithmetic_command.startswith('not'):
-            self.output_content += '// not\n@SP\nA=M\nM=!M\n'
+            self.output_content += '// not\n@SP\nA=M-1\nM=!M\n'
 
     def write_push_pop(self, command, segment, index):
         # TODO Write this function
@@ -53,7 +53,7 @@ class VMCodeWriter:
         elif command_type == 'eq':
             jump_type = 'JEQ'
 
-        self.output_content += '// ' + command_type + '\n@0\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@JUMP.'
+        self.output_content += '// ' + command_type + '\n@0\nM=M-1\nA=M\nD=M\nA=A-1\nD=M-D\n@JUMP.'
         self.output_content += str(self.jump_pointer) + '\nD;' + jump_type
         self.output_content += '\n@0\nD=A\n@SP\nA=M-1\nM=D\n@END.' + str(self.jump_pointer) + '\n0;JMP\n(JUMP.'
         self.output_content += str(self.jump_pointer) + ')\n@1\nD=-A\n@SP\nA=M-1\nM=D\n(END.' \
