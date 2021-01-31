881b2f8efdd19cb36e69035be1b8b5461385a72c
Hugo Barroca <hugobarroca98@gmail.com>
2021-01-31 22:35:36 +0000

    Added HashMap with keywords to JackTokenizer.

--- Project 10/src/main/JackTokenizer.java
+++ Project 10/src/main/JackTokenizer.java
@@ -6,17 +6,23 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class JackTokenizer {
 	private String currentToken;
 	private String fileContent;
+	private ArrayList<String[]> tokenList;
+	private HashMap<String, String> lexicalElements; //Value, LexicalElement
 
 	public JackTokenizer(File inputFile) throws FileNotFoundException {
 		currentToken = null;
+		lexicalElements = new HashMap<String, String>();
 		fileContent = "";
+		tokenList  = new ArrayList<String[]>();
+		populateLexicalElements();
 		readFileIntoString(inputFile);
-
 	}
 
 	private void readFileIntoString(File inputFile) throws FileNotFoundException {
@@ -30,11 +36,12 @@ public class JackTokenizer {
 
 			while (fileScanner.hasNextLine()) {
 				currentLine = fileScanner.nextLine();
-				if (!(currentLine.startsWith("//") | currentLine.startsWith("/**"))) {
+				if (!(currentLine.startsWith("//"))) {
 					lineScanner = new Scanner(currentLine);
 					while (lineScanner.hasNext()) {
 						currentWord = lineScanner.next();
 						if (!currentWord.equals("//")) {
+							stripTokens(currentWord);
 							fileContent += currentWord;
 							fileContent += " ";
 						} else {
@@ -50,9 +57,58 @@ public class JackTokenizer {
 		}
 	}
 
+	private void stripTokens(String tokens) {
+		//TODO: Implement method
+	}
+	
 	public boolean hasMoreTokens() {
 		// TODO: Implement method.
 		return true;
+	}
+	
+	private void populateLexicalElements() {
+		//Populate keywords
+		lexicalElements.put("class", "KEYWORD");
+		lexicalElements.put("constructor", "KEYWORD");
+		lexicalElements.put("function", "KEYWORD");
+		lexicalElements.put("method", "KEYWORD");
+		lexicalElements.put("field", "KEYWORD");
+		lexicalElements.put("static", "KEYWORD");
+		lexicalElements.put("var", "KEYWORD");
+		lexicalElements.put("int", "KEYWORD");
+		lexicalElements.put("char", "KEYWORD");
+		lexicalElements.put("boolean", "KEYWORD");
+		lexicalElements.put("void", "KEYWORD");
+		lexicalElements.put("true", "KEYWORD");
+		lexicalElements.put("false", "KEYWORD");
+		lexicalElements.put("null", "KEYWORD");
+		lexicalElements.put("this", "KEYWORD");
+		lexicalElements.put("let", "KEYWORD");
+		lexicalElements.put("do", "KEYWORD");
+		lexicalElements.put("if", "KEYWORD");
+		lexicalElements.put("else", "KEYWORD");
+		lexicalElements.put("while", "KEYWORD");
+		lexicalElements.put("return", "KEYWORD");
+		//Populate Symbols
+		lexicalElements.put("{", "SYMBOL");
+		lexicalElements.put("}", "SYMBOL");
+		lexicalElements.put("(", "SYMBOL");
+		lexicalElements.put(")", "SYMBOL");
+		lexicalElements.put("[", "SYMBOL");
+		lexicalElements.put("]", "SYMBOL");
+		lexicalElements.put(".", "SYMBOL");
+		lexicalElements.put(",", "SYMBOL");
+		lexicalElements.put(";", "SYMBOL");
+		lexicalElements.put("+", "SYMBOL");
+		lexicalElements.put("-", "SYMBOL");
+		lexicalElements.put("*", "SYMBOL");
+		lexicalElements.put("/", "SYMBOL");
+		lexicalElements.put("&", "SYMBOL");
+		lexicalElements.put("|", "SYMBOL");
+		lexicalElements.put("<", "SYMBOL");
+		lexicalElements.put(">", "SYMBOL");
+		lexicalElements.put("=", "SYMBOL");
+		lexicalElements.put("~", "SYMBOL");
 	}
 
 	public void printFileContent() {
