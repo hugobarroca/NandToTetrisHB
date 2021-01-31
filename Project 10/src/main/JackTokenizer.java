66f8d80676e33f7b58e12375c4026d7d47d469ef
Hugo Barroca <hugobarroca98@gmail.com>
2021-01-31 23:37:18 +0000

    Greatly improved populateTokenList() method.

--- Project 10/src/main/JackTokenizer.java
+++ Project 10/src/main/JackTokenizer.java
@@ -8,88 +8,106 @@ import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 public class JackTokenizer {
-	private String currentToken;
 	private String fileContent;
 	private ArrayList<String[]> tokenList;
-	private HashMap<String, String> lexicalElements; //Value, LexicalElement
+	private HashMap<String, String> lexicalElements; // Value, LexicalElement
 
 	public JackTokenizer(File inputFile) throws FileNotFoundException {
-		currentToken = null;
 		lexicalElements = new HashMap<String, String>();
 		fileContent = "";
-		tokenList  = new ArrayList<String[]>();
+		tokenList = new ArrayList<String[]>();
 		populateLexicalElements();
-		readFileIntoString(inputFile);
+		populateTokenList(inputFile);
 	}
 
-	private void readFileIntoString(File inputFile) throws FileNotFoundException {
+	private void populateTokenList(File inputFile) throws FileNotFoundException {
 		String currentLine = null;
 		String currentWord = null;
 		Scanner fileScanner = null;
-		Scanner lineScanner = null;
 
-		if (!inputFile.isDirectory()) {
-			fileScanner = new Scanner(inputFile);
-
-			while (fileScanner.hasNextLine()) {
-				currentLine = fileScanner.nextLine();
-				if (!(currentLine.startsWith("//"))) {
-					lineScanner = new Scanner(currentLine);
-					while (lineScanner.hasNext()) {
-						currentWord = lineScanner.next();
-						if (!currentWord.equals("//")) {
-							stripTokens(currentWord);
-							fileContent += currentWord;
-							fileContent += " ";
-						} else {
-							break;
-						}
-					}
-					lineScanner.close();
-				}
-			}
-			fileScanner.close();
-		} else {
+		if (inputFile.isDirectory()) {
 			System.out.println("ERROR: JackTokenizer was given a directory!");
+			System.exit(1);
 		}
+
+		fileScanner = new Scanner(inputFile);
+
+		// Iterate through every line in the file.
+		while (fileScanner.hasNextLine()) {
+			currentLine = fileScanner.nextLine();
+
+			if ((!(currentLine.startsWith("//"))))
+				continue;
+			stripTokens(currentWord);
+		}
+
+		fileScanner.close();
 	}
 
 	private void stripTokens(String tokens) {
-		//TODO: Implement method
+		Iterator it = lexicalElements.entrySet().iterator();
+		boolean tokenFound = false;
+		String[] keyValuePair = {};
+		String tempTokens = tokens;
+
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+
+			if (tempTokens.startsWith(pair.getValue().toString())) {
+				tempTokens.replace(pair.getValue().toString(), "");
+				keyValuePair[0] = pair.getKey().toString();
+				keyValuePair[1] = pair.getValue().toString();
+				tokenList.add(keyValuePair);
+				tokenFound = true;
+			}
+			it.remove();
+		}
+		if (!tokenFound) {
+			System.out.println("ERROR: Unrecognized token: " + tokens);
+			System.exit(1);
+		}
+		if (tempTokens != "") {
+			stripTokens(tempTokens);
+		} else {
+			return;
+		}
 	}
-	
+
 	public boolean hasMoreTokens() {
 		// TODO: Implement method.
 		return true;
 	}
-	
+
 	private void populateLexicalElements() {
-		//Populate keywords
-		lexicalElements.put("class", "KEYWORD");
+		// Populate keywords
+		lexicalElements.put("class",       "KEYWORD");
 		lexicalElements.put("constructor", "KEYWORD");
-		lexicalElements.put("function", "KEYWORD");
-		lexicalElements.put("method", "KEYWORD");
-		lexicalElements.put("field", "KEYWORD");
-		lexicalElements.put("static", "KEYWORD");
-		lexicalElements.put("var", "KEYWORD");
-		lexicalElements.put("int", "KEYWORD");
-		lexicalElements.put("char", "KEYWORD");
-		lexicalElements.put("boolean", "KEYWORD");
-		lexicalElements.put("void", "KEYWORD");
-		lexicalElements.put("true", "KEYWORD");
-		lexicalElements.put("false", "KEYWORD");
-		lexicalElements.put("null", "KEYWORD");
-		lexicalElements.put("this", "KEYWORD");
-		lexicalElements.put("let", "KEYWORD");
-		lexicalElements.put("do", "KEYWORD");
-		lexicalElements.put("if", "KEYWORD");
-		lexicalElements.put("else", "KEYWORD");
-		lexicalElements.put("while", "KEYWORD");
-		lexicalElements.put("return", "KEYWORD");
-		//Populate Symbols
+		lexicalElements.put("function",    "KEYWORD");
+		lexicalElements.put("method",      "KEYWORD");
+		lexicalElements.put("field",       "KEYWORD");
+		lexicalElements.put("static",      "KEYWORD");
+		lexicalElements.put("var",         "KEYWORD");
+		lexicalElements.put("int",         "KEYWORD");
+		lexicalElements.put("char",        "KEYWORD");
+		lexicalElements.put("boolean",     "KEYWORD");
+		lexicalElements.put("void",        "KEYWORD");
+		lexicalElements.put("true",        "KEYWORD");
+		lexicalElements.put("false",       "KEYWORD");
+		lexicalElements.put("null",        "KEYWORD");
+		lexicalElements.put("this",        "KEYWORD");
+		lexicalElements.put("let",         "KEYWORD");8
+		lexicalElements.put("do",          "KEYWORD");
+		lexicalElements.put("if",          "KEYWORD");
+		lexicalElements.put("else",        "KEYWORD");
+		lexicalElements.put("while",       "KEYWORD");
+		lexicalElements.put("return",      "KEYWORD");
+
+		// Populate Symbols
 		lexicalElements.put("{", "SYMBOL");
 		lexicalElements.put("}", "SYMBOL");
 		lexicalElements.put("(", "SYMBOL");
