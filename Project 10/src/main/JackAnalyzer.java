43df907ab5de94c79df77914af73568780968008
Hugo Barroca <hugobarroca98@gmail.com>
2021-02-15 20:20:18 +0000

    Refactored compileFileOrDirectory() method.

--- Project 10/src/main/JackAnalyzer.java
+++ Project 10/src/main/JackAnalyzer.java
@@ -5,43 +5,42 @@ import java.io.FileNotFoundException;
 
 public class JackAnalyzer {
 	String path;
-	
+
 	public JackAnalyzer(String path) {
 		this.path = path;
 	}
-	
-	
-	
-	public void compileFileOrDirectory() {
-		
-		File[] programFiles;
-		File programToParse;
-		
 
-		
+	public void compileFileOrDirectory() {
+		File programToParse;
+
 		System.out.println("Starting compilation process!");
-		
+
 		programToParse = new File(path);
-		
-		if (programToParse.isDirectory()) {
-			System.out.println("Parsing directory: " + programToParse.getAbsolutePath());
-			programFiles = programToParse.listFiles();
-			for (File programFile : programFiles) {
-				if (programFile.getName().endsWith(".jack"))
-					parseFile(programFile);
-			}
-		} else {
+
+		if (programToParse.isDirectory())
+			parseDirectory(programToParse);
+		else
 			parseFile(programToParse);
+
+	}
+
+	public static void parseDirectory(File programToParse) {
+		File[] programFiles;
+
+		System.out.println("Parsing directory: " + programToParse.getAbsolutePath());
+
+		programFiles = programToParse.listFiles();
+		for (File programFile : programFiles) {
+			if (programFile.getName().endsWith(".jack"))
+				parseFile(programFile);
 		}
 	}
-	
+
 	public static void parseFile(File programFile) {
 		try {
 			System.out.println("Attempting to process file: " + programFile.getAbsolutePath());
 			JackTokenizer tokenizer = new JackTokenizer(programFile);
 			tokenizer.generateXML(programFile.getAbsolutePath().replace("jack", "xml"));
-//			System.out.println("Printing token list:");
-//			tokenizer.printTokenList();
 		} catch (FileNotFoundException e) {
 			System.out.println("There was an issue opening the file!");
 			e.printStackTrace();
