aec1b89273c7ec880df93cce78f7135efa7d73d8
Hugo Barroca <hugobarroca98@gmail.com>
2021-02-15 20:10:53 +0000

    Refactored code from Main.java into JackAnalyzer.java

--- Project 10/src/main/JackAnalyzer.java
+++ Project 10/src/main/JackAnalyzer.java
@@ -1,5 +1,50 @@
 package main;
 
-public class JackAnalyzer {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class JackAnalyzer {
+	String path;
+	
+	public JackAnalyzer(String path) {
+		this.path = path;
+	}
+	
+	
+	
+	public void compileFileOrDirectory() {
+		
+		File[] programFiles;
+		File programToParse;
+		
+
+		
+		System.out.println("Starting compilation process!");
+		
+		programToParse = new File(path);
+		
+		if (programToParse.isDirectory()) {
+			System.out.println("Parsing directory: " + programToParse.getAbsolutePath());
+			programFiles = programToParse.listFiles();
+			for (File programFile : programFiles) {
+				if (programFile.getName().endsWith(".jack"))
+					parseFile(programFile);
+			}
+		} else {
+			parseFile(programToParse);
+		}
+	}
+	
+	public static void parseFile(File programFile) {
+		try {
+			System.out.println("Attempting to process file: " + programFile.getAbsolutePath());
+			JackTokenizer tokenizer = new JackTokenizer(programFile);
+			tokenizer.generateXML(programFile.getAbsolutePath().replace("jack", "xml"));
+//			System.out.println("Printing token list:");
+//			tokenizer.printTokenList();
+		} catch (FileNotFoundException e) {
+			System.out.println("There was an issue opening the file!");
+			e.printStackTrace();
+		}
+	}
 }
--- Project 10/src/main/Main.java
+++ Project 10/src/main/Main.java
@@ -1,47 +1,16 @@
 package main;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-
 public class Main {
-	
-	// Main method of the application
-	public static void main(String[] args) {
-		File[] programFiles;
-		File programToParse;
 
+	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.out.println("No arguments were given! Quitting program!");
 			System.exit(1);
 		}
+		
+		JackAnalyzer analyzer = new JackAnalyzer(args[0]);
+		analyzer.compileFileOrDirectory();
 
-		System.out.println("Starting compilation process!");
-
-		programToParse = new File(args[0]);
-
-		if (programToParse.isDirectory()) {
-			System.out.println("Parsing directory: " + programToParse.getAbsolutePath());
-			programFiles = programToParse.listFiles();
-			for (File programFile : programFiles) {
-				if (programFile.getName().endsWith(".jack"))
-					parseFile(programFile);
-			}
-		} else {
-			parseFile(programToParse);
-		}
 	}
 
-	
-	public static void parseFile(File programFile) {
-		try {
-			System.out.println("Attempting to process file: " + programFile.getAbsolutePath());
-			JackTokenizer tokenizer = new JackTokenizer(programFile);
-			tokenizer.generateXML(programFile.getAbsolutePath().replace("jack", "xml"));
-//			System.out.println("Printing token list:");
-//			tokenizer.printTokenList();
-		} catch (FileNotFoundException e) {
-			System.out.println("There was an issue opening the file!");
-			e.printStackTrace();
-		}
-	}
 }
